93e63aa811703e2a4b134f40ab5cd690c3a5010f
mustapha bousfina <bousfinamustapha@gmail.com>
2023-08-27 18:55:37 +0100

    intro to routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'main page';
 });
+
+Route::get('/hello', function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+Route::fallback(function () {
+    return 'Still got Somewhere!';
+});
+
