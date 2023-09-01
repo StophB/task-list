cc748e725fddb6f480eef5c0739b233a9fc6d62f
mustapha bousfina <bousfinamustapha@gmail.com>
2023-09-02 00:17:30 +0100

    edited

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,23 @@
 
 @section('content')
     <div>
+        <a href="{{ route('tasks.create') }}">Add Task!</a>
+    </div>
+    <div>
         @forelse ($tasks as $task)
             <div>
-                <a href="{{route('tasks.show', ['id' => $task->id])}}">
+                <a href="{{route('tasks.show', ['task' => $task->id])}}">
                     {{ $task->title }}
                 </a>
             </div>
         @empty
             <div>There are no tasks!</div>
         @endforelse
+</div>
+
+<div>
+    @if ($tasks->count())
+        {{ $tasks->links() }}
+    @endif
 </div>
 @endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,5 +11,31 @@
 
     <p>{{ $task->created_at }}</p>
     <p>{{ $task->updated_at }}</p>
+
+    <p>
+        @if ($task->completed)
+            Completed!
+        @else
+            Not Completed!
+        @endif
+    </p>
+
+    <form action="{{ route('tasks.toggle-complete', ['task' => $task]) }}" method="post">
+        @csrf
+        @method('PUT')
+        <button type="submit">
+            Mask as {{ $task->completed ? 'not completed' : 'completed' }}
+        </button>
+    </form>
+
+    <div>
+        <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit Task</a>
+    </div>
+
+    <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="post">
+        @csrf
+        @method('DELETE')
+        <button type="submit">Delete</button>
+    </form>
 @endsection
 
