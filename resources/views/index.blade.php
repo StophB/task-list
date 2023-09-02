59b1db23a17880fd39b201f1ea29c2f41550d450
mustapha bousfina <bousfinamustapha@gmail.com>
2023-09-02 02:30:28 +0100

    Add tailwindcss styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,14 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-    <div>
-        <a href="{{ route('tasks.create') }}">Add Task!</a>
-    </div>
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}" class="link">Add Task!</a>
+    </nav>
     <div>
         @forelse ($tasks as $task)
             <div>
-                <a href="{{route('tasks.show', ['task' => $task->id])}}">
+                <a href="{{route('tasks.show', ['task' => $task->id])}}"
+                    @class(['line-through' => $task->completed])>
                     {{ $task->title }}
                 </a>
             </div>
@@ -18,9 +19,9 @@
         @endforelse
 </div>
 
-<div>
+<nav class="mt-4">
     @if ($tasks->count())
         {{ $tasks->links() }}
     @endif
-</div>
+</nav>
 @endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,39 +3,45 @@
 @section('title', $task->title)
 
 @section('content')
-    <p>{{ $task->description }}</p>
+
+    <div class="mb-4">
+        <a href="{{ route('tasks.index') }}" class="link">← Go back to the tasks list!</a>
+    </div>
+
+    <p class="mb-4 text-slate-700">{{ $task->description }}</p>
 
     @if ($task->long_description)
-        <p>{{ $task->long_description }}</p>
+        <p class="mb-4 text-slate-700">{{ $task->long_description }}</p>
     @endif
 
-    <p>{{ $task->created_at }}</p>
-    <p>{{ $task->updated_at }}</p>
+    <p class="mb-4 text-sm text-slate-500">Created {{ $task->created_at->diffForHumans() }} • Updated {{ $task->updated_at->diffForHumans() }}</p>
 
-    <p>
+    <p class="mb-4">
         @if ($task->completed)
-            Completed!
+            <span class="font-medium text-green-500">Completed!</span>
         @else
-            Not Completed!
+            <span class="font-medium text-red-500">Not Completed!</span>
         @endif
     </p>
 
-    <form action="{{ route('tasks.toggle-complete', ['task' => $task]) }}" method="post">
-        @csrf
-        @method('PUT')
-        <button type="submit">
-            Mask as {{ $task->completed ? 'not completed' : 'completed' }}
-        </button>
-    </form>
+    <div class="flex gap-2">
+        <a href="{{ route('tasks.edit', ['task' => $task]) }}"
+            class="btn">Edit Task</a>
 
-    <div>
-        <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit Task</a>
+
+        <form action="{{ route('tasks.toggle-complete', ['task' => $task]) }}" method="post">
+            @csrf
+            @method('PUT')
+            <button type="submit" class="btn">
+                Mask as {{ $task->completed ? 'not completed' : 'completed' }}
+            </button>
+        </form>
+
+        <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn">Delete</button>
+        </form>
     </div>
-
-    <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="post">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Delete</button>
-    </form>
 @endsection
 
